d7681b4f69dcd2f3ea79bca03c6dca299153d713
Emzawadzki <emzawadzki@gmail.com>
2020-07-21 22:27:57 +0200

    Add db create

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-print("Pick one of the options below:\n1) Create new record\nq) Exit")
+from crud.create import create_record
+
+pick_text = "Pick one of the options below:\n1) Create new record\nq) Exit"
 is_running = True
 
 
 def handle_choice(choice):
-    print("You picked " + choice + "!")
     if choice == "q":
         print("Closing program ...")
         exit()
+    elif choice == "1":
+        create_record()
+    else:
+        print("Unknown input.\n" + pick_text)
 
 
 while is_running == True:
+    print(pick_text)
     option = input()
     handle_choice(option)
