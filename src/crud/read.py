1a920a1d1137947bcb11fb23cac59d35bb9fd059
Emzawadzki <emzawadzki@gmail.com>
2020-07-23 00:17:51 +0200

    Add read functionality

--- src/crud/read.py
+++ src/crud/read.py
@@ -1,12 +1,24 @@
-import csv
+import mysql.connector
+from mysql.connector import Error
 
-from globals import db_fields, db_file
+from globals import connection_config, main_table_name
 
 
 def read_all_records():
-    with open(db_file, "r") as csv_file:
-        reader = csv.DictReader(csv_file, fieldnames=db_fields)
-        next(reader)
-        print("\nDatabase records:")
-        for row in reader:
-            print(row["first_name"] + " " + row["last_name"])
+    try:
+        connection = mysql.connector.connect(**connection_config)
+        if connection.is_connected():
+            print("[INFO] Connected to MySQL Server")
+            cursor = connection.cursor()
+            query = "SELECT * FROM " + main_table_name
+            cursor.execute(query)
+            result = cursor.fetchall()
+            for _, first_name, last_name in result:
+                print(first_name + " " + last_name)
+    except Error as e:
+        print("[INFO] Error while connecting to MySQL", e)
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("[INFO] MySQL connection closed")
